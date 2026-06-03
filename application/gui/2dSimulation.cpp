579222deab20c268d12310942a624214ac2b82f2
wal1624 <waliaro24@gmail.com>
2026-06-02 22:34:52 -0400

    Started making a cpp file for the first time

---
+++ application/gui/2dSimulation.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file 2dSimulation.cpp
+ * @brief Implementation of my GUI to run a 2d simulation for my Slam Project
+ * 
+ * Trying to create production level code for my SLAM proeject. Going to create a GUI program in this file
+ * \
+
+
+#include "gui.cpp"
+
+#include <iostream>
+
+
+namespace gui {
+
+
+}
